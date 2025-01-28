4d4d5f1aabf79596695ad31de764dc982a3086d4
joaoreberti <57142417+joaoreberti@users.noreply.github.com>
2025-01-28 01:02:30 +0100

    feat: grpc server is working with typescript types

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const eslintTypescriptRecommended = require("@typescript-eslint/eslint-plugin");
 
 module.exports = [
   // Any other config imports go at the top
   eslintPluginPrettierRecommended,
+  eslintTypescriptRecommended,
   {
     files: ["src/**/*.js", "src/**/*.ts", "./eslint.config.js"], // Include JavaScript and TypeScript files in the src directory,
     ignores: ["src/proto/*"], // Exclude test files
--- src/app/server.ts
+++ src/app/server.ts
@@ -1,14 +1,59 @@
-import Fastify from "fastify";
+import * as grpc from "@grpc/grpc-js";
+import { loadSync } from "@grpc/proto-loader";
+import * as path from "path";
+import {
+  getShoppingListRequest,
+  getShoppingListResponse,
+} from "../proto/shopping_list";
 
-const fastify = Fastify({
-  logger: true,
+// Load the protobuf
+const packageDefinition = loadSync(
+  path.resolve(__dirname, "../../proto/shopping_list.proto"),
+  {
+    keepCase: false,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+  }
+);
+
+// Load the package definition
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition) as any;
+
+const shoppingListProto = protoDescriptor.ShoppingList;
+
+const getShoppingList = (
+  call: grpc.ServerUnaryCall<getShoppingListRequest, any>,
+  callback: grpc.sendUnaryData<getShoppingListResponse>
+) => {
+  const request = call.request;
+  console.log(`Getting shopping list: ${request.id}`);
+
+  const response: getShoppingListResponse = {
+    currentItems: ["Milk", "Bread"],
+  };
+
+  console.log("Response:", response);
+
+  callback(null, response);
+};
+
+// Create a new gRPC server
+const server = new grpc.Server();
+server.addService(shoppingListProto.service, {
+  getShoppingList: getShoppingList,
 });
 
-fastify.get("/", async (request, reply) => {
-  reply.type("application/json").code(200);
-  return { hello: "world" };
-});
-
-fastify.listen({ port: 3000 }, (err, address) => {
-  if (err) throw err;
-});
+// Start the server
+server.bindAsync(
+  "0.0.0.0:50051",
+  grpc.ServerCredentials.createInsecure(),
+  (err, port) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`Server running on 0.0.0.0:${port}`);
+  }
+);
